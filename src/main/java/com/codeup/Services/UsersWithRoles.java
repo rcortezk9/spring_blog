2cf6227e582dcc0164ae5085a1bb7333a07fb479
rcortezk9 <rcortezk9@gmail.com>
2017-06-26 11:18:51 -0500

    Added the dependencies to the pom file. - Created the Model for UserRoles. - Created the repo for Roles. - Created the service for the security files

---
+++ src/main/java/com/codeup/Services/UsersWithRoles.java
@@ -0,0 +1,50 @@
+package com.codeup.Services;
+
+import com.codeup.Repositories.Roles;
+import com.codeup.models.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Created by renecortez on 6/26/17.
+ */
+
+public class UsersWithRoles extends User implements UserDetails {
+    private List<String> userRoles;
+
+    public UsersWithRoles(User user, List<String> userRoles) {
+        super(user);
+        this.userRoles = userRoles;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
+
+
